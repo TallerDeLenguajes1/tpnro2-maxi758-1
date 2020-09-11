9a2df08c94fa53480d95e52a2fd2fa38284173ae
maxi758 <maxi758@gmail.com>
2020-09-11 17:58:16 -0300

    cuadrado

--- tpno2/Program.cs
+++ tpno2/Program.cs
@@ -6,7 +6,24 @@ namespace tpno2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Realizar una aplicación de consola que cargue de un número entero por teclado e imprimir su cuadrado
+
+            int numero;
+            String aux;
+            try
+            {
+                Console.WriteLine("Escriba el numero del cual quiere saber su cuadrado");
+                aux = Console.ReadLine();
+                numero=Int32.Parse(aux);
+                Console.Write(numero * numero);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Se produjo un error");
+                Console.WriteLine(ex.ToString());
+               // throw;
+            }
+            Console.ReadKey();
         }
     }
 }
